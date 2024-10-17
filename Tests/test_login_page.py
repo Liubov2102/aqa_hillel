f14f64ef10a003ce0a326a2d2a07b74d6b0779bd
Liubov_Lys <osmilovska@gmail.com>
2024-10-17 16:43:05 +0300

    logout

--- Tests/test_login_page.py
+++ Tests/test_login_page.py
@@ -1,6 +1,11 @@
+from threading import active_count
+
+
 def test_login():
     # Open login page
     # Enter email
     # Enter password
     # Click login
     pass
+def logout
+    Click my active_count()
